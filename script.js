49ec0f2e159e35a5c3e3ef512eeddf0268d5bc38
fvelarde <50224654+fvelarde@users.noreply.github.com>
2024-06-12 12:23:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 // Get the container element
 const imageContainer = document.getElementById('image-container');
 
-// Get the folder path
-const folderPath = 'plots/'; // Update this to match your folder path
+// GitHub API endpoint
+const apiUrl = 'https://api.github.com/repos/fvelarde/chc_online/contents/plots';
 
-// Get the files in the folder
-fetch(folderPath)
+// Fetch the contents of the plots folder
+fetch(apiUrl)
     .then(response => response.json())
-    .then(files => {
+    .then(data => {
         // Loop through the files and create image elements
-        files.forEach(file => {
-            const img = document.createElement('img');
-            img.src = folderPath + file;
-            img.alt = file;
-            imageContainer.appendChild(img);
+        data.forEach(file => {
+            if (file.type === 'file') {
+                const img = document.createElement('img');
+                img.src = file.download_url;
+                img.alt = file.name;
+                imageContainer.appendChild(img);
+            }
         });
     })
     .catch(error => console.error(error));
